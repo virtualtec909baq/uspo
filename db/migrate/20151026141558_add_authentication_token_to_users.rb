a64b4211bbd17a98e1acaf975e70dd6f75013670
jquiu <jenniffer54@gmail.com>
2015-10-27 08:51:48 -0500

    rm confirmed_at

--- db/migrate/20151026141558_add_authentication_token_to_users.rb
+++ db/migrate/20151026141558_add_authentication_token_to_users.rb
@@ -1,9 +1,6 @@
 class AddAuthenticationTokenToUsers < ActiveRecord::Migration
   def change
     add_column :users, :authentication_token, :string
-    add_column :users, :confirmation_token, :string
-    add_column :users, :tokens, :string
-    add_column :users, :confirmation_sent_at, :datetime
     add_index :users, :authentication_token
   end
 end
