df18e03472683250c92282c3b481a6d3f5a81873
jquiu <jenniffer54@gmail.com>
2015-10-29 11:50:28 -0500

    format dates validate times

--- app/models/courier.rb
+++ app/models/courier.rb
@@ -2,4 +2,6 @@ class Courier < ActiveRecord::Base
 	has_many :couriers, :through => :packeges
   	has_many :packeges 
   	belongs_to :user
+  	validates :time_arriv, presence: true
+  	validates :departure_time, presence: true
 end
