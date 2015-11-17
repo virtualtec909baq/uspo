e1ecca4dcef393986a5d4b81e64809dc0943e646
jquiu <jenniffer54@gmail.com>
2015-11-17 11:21:32 -0500

    uploaders

---
+++ app/uploaders/image_uploader.rb
@@ -0,0 +1,26 @@
+# encoding: utf-8
+
+class ImageUploader < CarrierWave::Uploader::Base
+
+  # Include RMagick or MiniMagick support:
+  # include CarrierWave::RMagick
+  # include CarrierWave::MiniMagick
+
+  # Choose what kind of storage to use for this uploader:
+  storage :file
+  
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  # version :thumb do
+  #   process :resize_to_fit => [108, 81]
+  # end
+
+  # Add a white list of extensions which are allowed to be uploaded.
+  # For images you might use something like this:
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
+
+end
