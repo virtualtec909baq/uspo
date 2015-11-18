96bcff68c8dcee936a1ac0213b8feee0fe107078
jquiu <jenniffer54@gmail.com>
2015-11-18 16:22:28 -0500

    rm validate

--- app/controllers/api/home_controller.rb
+++ app/controllers/api/home_controller.rb
@@ -21,17 +21,14 @@ class Api::HomeController < ApplicationController
 
 	def edit_profile
 		@user = []
-		if user = User.find(params[:id]) 
-			if user.update(user_params)
-				user_array = ["id", "#{user.id}","name", "#{user.name} #{user.last_name}","pic","#{get_user_photo(user)}","phone", "#{user.phone}","age", "#{user.age}" ,"email", "#{user.email}", "bio" ,"#{user.bio}", "city", "#{user.city}", "fb", "#{user.fb}", "tw", "#{user.tw}", "int", "#{user.int}"]
-				user_hash = Hash[*user_array]
-				@user << user_hash
-				render json: { user: @user, status: "ok" },status: 200
-			else
-				render json: { user: @user.errors, status: "not_found" },status: 422
-			end
+		user = User.find(params[:id]) 
+		if user.update(user_params)
+			user_array = ["id", "#{user.id}","name", "#{user.name} #{user.last_name}","pic","#{get_user_photo(user)}","phone", "#{user.phone}","age", "#{user.age}" ,"email", "#{user.email}", "bio" ,"#{user.bio}", "city", "#{user.city}", "fb", "#{user.fb}", "tw", "#{user.tw}", "int", "#{user.int}"]
+			user_hash = Hash[*user_array]
+			@user << user_hash
+			render json: { user: @user, status: "ok" },status: 200
 		else
-			record_not_found(error)
+			render json: { user: @user.errors, status: "not_found" },status: 422
 		end
 	end
 	
