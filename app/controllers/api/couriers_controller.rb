edceeba6ebb941ef31e88e1abbb82bdd32851206
jquiu <jenniffer54@gmail.com>
2015-12-03 10:11:41 -0500

    format dates

--- app/controllers/api/couriers_controller.rb
+++ app/controllers/api/couriers_controller.rb
@@ -12,7 +12,7 @@ class Api::CouriersController < ApplicationController
     end
     @couriers_list = []
     @couriers.each do |courier|
-      a = ["id", "#{courier.id}", "user_id", "#{courier.user_id}", "user", "#{courier.user.name}","trip_description", "#{courier.trip_description}", "location_arrived", "#{courier.location_arrived}", "location_departure", "#{courier.location_departure}", "time_arriv", "#{courier.time_arriv.strftime("%B %d %Y %I:%M%p")}", "departure_time", "#{courier.departure_time.strftime("%B %d %Y %I:%M%p")}"]
+      a = ["id", "#{courier.id}", "user_id", "#{courier.user_id}", "user", "#{courier.user.name}","trip_description", "#{courier.trip_description}", "location_arrived", "#{courier.location_arrived}", "location_departure", "#{courier.location_departure}", "time_arriv", "#{courier.time_arriv.strftime("%B %d %Y %I:%M%p")}", "departure_time", "#{courier.departure_time.strftime("%b %d %I:%M%p")}"]
       h = Hash[*a]
       @couriers_list << h
     end
