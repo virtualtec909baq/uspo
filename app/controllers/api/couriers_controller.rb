4abb749264dab4117681f7aa244539fcedfeeb83
jquiu <jenniffer54@gmail.com>
2015-12-03 10:25:28 -0500

    set user image

--- app/controllers/api/couriers_controller.rb
+++ app/controllers/api/couriers_controller.rb
@@ -1,7 +1,7 @@
 class Api::CouriersController < ApplicationController
   before_action :set_courier, only: [:show, :edit, :update, :destroy]
   rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
-  
+  include ApplicationHelper
   # GET /couriers
   def index
     if params[:q]
@@ -12,7 +12,7 @@ class Api::CouriersController < ApplicationController
     end
     @couriers_list = []
     @couriers.each do |courier|
-      a = ["id", "#{courier.id}", "user_id", "#{courier.user_id}", "user", "#{courier.user.name}","trip_description", "#{courier.trip_description}", "location_arrived", "#{courier.location_arrived}", "location_departure", "#{courier.location_departure}", "time_arriv", "#{courier.time_arriv.strftime("%B %d %Y %I:%M%p")}", "departure_time", "#{courier.departure_time.strftime("%b %d %I:%M%p")}"]
+      a = ["id", "#{courier.id}", "user_id", "#{courier.user_id}", "user", "#{courier.user.name}","pic", "#{get_user_photo(courier.user)}" , "trip_description", "#{courier.trip_description}", "location_arrived", "#{courier.location_arrived}", "location_departure", "#{courier.location_departure}", "time_arriv", "#{courier.time_arriv.strftime("%B %d %Y %I:%M%p")}", "departure_time", "#{courier.departure_time.strftime("%b %d %I:%M%p")}"]
       h = Hash[*a]
       @couriers_list << h
     end
