fa679449492c66e0bb81251bd55c8053339389c3
jquiu <jenniffer54@gmail.com>
2016-01-06 16:32:10 -0500

    change fields db

--- db/migrate/20160106212859_add_fields_to_couriers.rb
+++ db/migrate/20160106212859_add_fields_to_couriers.rb
@@ -1,4 +1,13 @@
 class AddFieldsToCouriers < ActiveRecord::Migration
   def change
+  	add_column :couriers, :zip_code, :integer
+    add_column :remittents, :zip_code, :integer
+    add_column :packages, :name, :string
+    add_column :packages, :city, :string
+    add_column :packages, :zipe_code, :integer
+    add_column :packages, :place, :string
+    add_column :packages, :date, :datetime
+    add_column :packages, :description, :string
+    add_column :packages, :phone, :string
   end
 end
