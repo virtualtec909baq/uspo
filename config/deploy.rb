69940cd0e16d4e73389990571c115f5e8b3161b6
jquiu <jenniffer54@gmail.com>
2016-01-13 14:43:05 -0500

    deploy copy capistanos

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,13 +10,14 @@ set :keep_releases, 3
 # files we want symlinking to specific entries in shared.
 set :linked_files, %w{config/database.yml}
 set :ssh_options, { :forward_agent => true }
-
+set :deploy_via, :copy
 # dirs we want symlinking to shared
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 set :default_env, { path: "~/.rbenv/shims:~/.rbenv/bin:$PATH" }
 # what specs should be run before deployment is allowed to
 # continue, see lib/capistrano/tasks/run_tests.cap
 set :tests, []
+
 
 # which config files should be copied by deploy:setup_config
 # see documentation in lib/capistrano/tasks/setup_config.cap
