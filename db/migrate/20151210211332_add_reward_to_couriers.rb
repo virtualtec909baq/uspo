6bfdd37783e2ebd4bb85f6b650a729788ca178fe
jquiu <jenniffer54@gmail.com>
2016-01-13 16:45:23 -0500

    rm rewards db

--- db/migrate/20151210211332_add_reward_to_couriers.rb
+++
@@ -1,6 +0,0 @@
-class AddRewardToCouriers < ActiveRecord::Migration
-  def change
-    add_column :couriers, :reward, :float
-    add_column :remittents, :reward, :float
-  end
-end
--- db/migrate/20160106213706_add_fields_reward.rb
+++
@@ -1,6 +0,0 @@
-class AddFieldsReward < ActiveRecord::Migration
-  def change
-  	add_column :couriers, :reward, :float
-    add_column :remittents, :reward, :float
-  end
-end
